09baeb7ba73731fdf34d1837ada3023648adac9d
danthe3rd <danthe3rd@users.noreply.github.com>
2020-02-01 17:33:44 +0100

    Re-enable test for json fetcher in windows

--- hiplot/test_fetchers.py
+++ hiplot/test_fetchers.py
@@ -5,7 +5,7 @@
 from pathlib import Path
 import json
 import tempfile
-import platform
+import shutil
 import pytest
 from . import experiment as exp
 from .fetchers import load_demo, load_csv, load_json, README_DEMOS
@@ -29,16 +29,22 @@ def test_fetcher_csv() -> None:
     with pytest.raises(exp.ExperimentFetcherDoesntApply):
         load_csv("file_does_not_exist.csv")
 
-@pytest.mark.skipif(platform.system() == "Windows",  # type: ignore
-                    reason="needs rewrite on windows")
+
 def test_fetcher_json() -> None:
-    with tempfile.NamedTemporaryFile("w+", suffix=".json") as tmpf:
-        json.dump([{"id": 1, "metric": 1.0, "param": "abc"}, {"id": 2, "metric": 1.0, "param": "abc", "option": "def"}], tmpf)
-        tmpf.flush()
-        xp = load_json(tmpf.name)
+    dirpath = tempfile.mkdtemp()
+    try:
+        json_path = dirpath + "/xp.json"
+        with Path(json_path).open("w+", encoding="utf-8") as tmpf:
+            json.dump([{"id": 1, "metric": 1.0, "param": "abc"}, {"id": 2, "metric": 1.0, "param": "abc", "option": "def"}], tmpf)
+        xp = load_json(json_path)
         xp.validate()
         assert xp.datapoints
         assert len(xp.datapoints) == 2
+    finally:
+        shutil.rmtree(dirpath)
+
+
+def test_fetcher_json_doesnt_apply() -> None:
     with pytest.raises(exp.ExperimentFetcherDoesntApply):
         load_json("something_else")
 
