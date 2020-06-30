8ec9dcdbb1eba0ad00533ebddd96418d5276b00a
ci-build <ci-build>
2020-06-30 10:40:34 +0000

    [skip ci] Updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.14',
+    VERSION: '0.1.15',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
