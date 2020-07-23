1587064bee36de6d447f7faec3b4f65ad8342faa
danthe3rd <danthe3rd@users.noreply.github.com>
2020-07-23 12:39:37 +0200

    Fix versioning for hiplot-master

--- hiplot/__init__.py
+++ hiplot/__init__.py
@@ -5,12 +5,12 @@
 from .experiment import (Experiment, ExperimentFetcherDoesntApply, ExperimentValidationError, ExperimentValidationCircularRef,
                          ExperimentValidationMissingParent, Datapoint, ExperimentDisplayed, ValueDef, ValueType, Displays)
 from .server import run_server, run_server_main
-from .pkginfo import version as __version__
+from .pkginfo import version as __version__, package_name
 
 from . import fetchers
 
 __all__ = [
     'Experiment', 'ExperimentFetcherDoesntApply', 'ExperimentValidationError', 'ExperimentValidationCircularRef',
     'ExperimentValidationMissingParent', 'Datapoint', 'ExperimentDisplayed', 'ValueDef', 'ValueType', 'Displays',
-    'fetchers', 'run_server', 'run_server_main', "__version__", "__package__"
+    'fetchers', 'run_server', 'run_server_main', "__version__", "__package__", "package_name"
 ]
