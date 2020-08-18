09c1c5304a8b973532dff3d9568a26a7e5a34b10
danthe3rd <danthe3rd@users.noreply.github.com>
2020-08-18 16:27:17 +0200

    Fix axis labels and tooltips in Webkit brwosers (safari...)

--- src/lib/svghelpers.ts
+++ src/lib/svghelpers.ts
@@ -11,29 +11,38 @@ import { ParamDef } from "../infertypes";
 import style from "../hiplot.scss";
 import { ContextMenu } from "../contextmenu";
 
+function leftPos(anchor: string, w: number): number {
+    return {
+        end: -w,
+        start: 0,
+        middle: -w / 2,
+    }[anchor];
+}
 export function foDynamicSizeFitContent(fo: SVGForeignObjectElement) {
-    const w = Math.floor(fo.children[0].clientWidth + 2); // borders 2 px
-    const h = Math.floor(fo.children[0].clientHeight + 2);
+    const TOOLTIP_WIDTH_PX = 80;
+    const w = Math.floor(fo.children[0].children[0].clientWidth + 2); // borders 2 px
+    const h = Math.floor(fo.children[0].children[0].clientHeight + 2);
     const anchor = fo.getAttribute("text-anchor");
-    if (anchor == "end") {
-        fo.setAttribute("x", `${-w}`);
-    } else if (anchor == "start") {
-        fo.setAttribute("x", "0");
-    } else if (anchor == "middle") {
-        fo.setAttribute("x", `${- w / 2}`)
-    }
+    const tooltip = fo.children[0].children[1] as HTMLSpanElement;
+    const anchor_x = leftPos(anchor, w);
+    fo.setAttribute("x", `${anchor_x}`);
+    // Set tooltip
+    const tooltip_anchor_x = leftPos(anchor, TOOLTIP_WIDTH_PX) - anchor_x;
+    const tooltip_width = Math.min(TOOLTIP_WIDTH_PX, TOOLTIP_WIDTH_PX - tooltip_anchor_x);
+    tooltip.style.marginLeft = `${tooltip_anchor_x}px`;
+    tooltip.style.width = `${tooltip_width}px`;
     fo.style.width = `${w}px`;
     fo.style.height = `${h}px`;
     fo.style.overflow = "visible";
 }
 
 export function foCreateAxisLabel(pd: ParamDef, cm?: React.RefObject<ContextMenu>, tooltip: string = "Right click for options"): SVGForeignObjectElement {
-    const fo = document.createElementNS('http://www.w3.org/2000/svg',"foreignObject");
-    const sel = d3.select(fo).append("xhtml:span")
-        .attr("class", pd.label_css)
+    var fo = document.createElementNS('http://www.w3.org/2000/svg',"foreignObject");
+    const span = d3.select(fo).append("xhtml:div")
         .classed(style.tooltipContainer, true)
+        .classed(style.label, true);
+    span.append("xhtml:span").attr("class", pd.label_css)
         .classed("d-inline-block", true)
-        .classed(style.label, true)
         .html(pd.name)
         .on("contextmenu", function() {
             if (cm) {
@@ -41,11 +50,12 @@ export function foCreateAxisLabel(pd: ParamDef, cm?: React.RefObject<ContextMenu
                 d3.event.preventDefault();
                 d3.event.stopPropagation();
             }
-        })
+        });
     if (tooltip) {
-        sel.append("span")
+        span.append("span")
             .classed(style.tooltiptext, true)
             .classed(style.tooltipBot, true)
+            .classed("d-inline-block", true)
             .text(tooltip);
     }
     return fo;
