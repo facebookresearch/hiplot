5c70066095bef2c5f26d5f212781e77ad61eda4a
danthe3rd <danthe3rd@users.noreply.github.com>
2020-11-26 15:29:08 +0100

    Columns with long names with spaces

--- src/lib/svghelpers.ts
+++ src/lib/svghelpers.ts
@@ -11,24 +11,32 @@ import { ParamDef } from "../infertypes";
 import style from "../hiplot.scss";
 import { ContextMenu } from "../contextmenu";
 
-function leftPos(anchor: string, w: number): number {
-    return {
+function leftPos(anchor: string, w: number, minmax?: [number, number]): number {
+    var left = {
         end: -w,
         start: 0,
         middle: -w / 2,
     }[anchor];
+    if (minmax) {
+        if (left < minmax[0]) {
+            left = minmax[0];
+        } else if (left + w > minmax[1]) {
+            left = minmax[1] - w;
+        }
+    }
+    return left;
 }
-export function foDynamicSizeFitContent(fo: SVGForeignObjectElement) {
+export function foDynamicSizeFitContent(fo: SVGForeignObjectElement, minmax?: [number, number]) {
     const TOOLTIP_WIDTH_PX = 80;
     const w = Math.floor(fo.children[0].children[0].clientWidth + 2); // borders 2 px
     const h = Math.floor(fo.children[0].children[0].clientHeight + 2);
     const anchor = fo.getAttribute("text-anchor");
     const tooltip = fo.children[0].children[1] as HTMLSpanElement;
-    const anchor_x = leftPos(anchor, w);
+    const anchor_x = leftPos(anchor, w, minmax);
     fo.setAttribute("x", `${anchor_x}`);
     // Set tooltip
     if (tooltip) {
-        const tooltip_anchor_x = leftPos(anchor, TOOLTIP_WIDTH_PX) - anchor_x;
+        const tooltip_anchor_x = leftPos(anchor, TOOLTIP_WIDTH_PX, minmax) - anchor_x;
         const tooltip_width = Math.min(TOOLTIP_WIDTH_PX, TOOLTIP_WIDTH_PX - tooltip_anchor_x);
         tooltip.style.marginLeft = `${tooltip_anchor_x}px`;
         tooltip.style.width = `${tooltip_width}px`;
@@ -43,7 +51,9 @@ export function foCreateAxisLabel(pd: ParamDef, cm?: React.RefObject<ContextMenu
     const span = d3.select(fo).append("xhtml:div")
         .classed(style.tooltipContainer, true)
         .classed(style.label, true);
-    span.append("xhtml:span").attr("class", pd.label_css)
+    span.append("xhtml:span")
+        .attr("class", pd.label_css)
+        .classed(style.axisLabelText, true)
         .classed("d-inline-block", true)
         .html(pd.name)
         .on("contextmenu", function() {
